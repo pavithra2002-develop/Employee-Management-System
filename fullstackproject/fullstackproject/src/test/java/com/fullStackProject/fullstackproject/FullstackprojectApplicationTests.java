8009b05272908171a4fd8b25b8d1c6de1a40589b
pavithra2002-develop <pavithra.jerripothula171191@gmail.com>
2025-03-13 11:11:40 +0530

    Add files via upload

---
+++ fullstackproject/fullstackproject/src/test/java/com/fullStackProject/fullstackproject/FullstackprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStackProject.fullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
